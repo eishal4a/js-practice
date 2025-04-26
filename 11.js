71d4735ba7bf8b80317e35e1db7112e1e9681f5c
Eishal <eishal.cecos@gmail.com>
2025-04-26 20:42:09 +0500

    p
    p

--- 11.js
+++ 11.js
@@ -1,10 +1,10 @@
-let array=[1,2,2,3,4,4,5];
-alert(array);
-let a=[];
-for(let i=1;i<array.length;i++){
-    
-    if(!a.includes(array[i])){
-        a.push(array);
+let array = [1, 2, 2, 3, 4, 4, 5];
+let uniqueArray = [];
+
+for (let i = 0; i < array.length; i++) {
+    if (!uniqueArray.includes(array[i])) {
+        uniqueArray.push(array[i]);
     }
 }
-alert(a);
+
+alert(uniqueArray);
