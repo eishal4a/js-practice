90e6bff3059a6be037da50d4f917b233beff72d2
Eishal <eishal.cecos@gmail.com>
2025-05-01 20:32:05 +0500

    p
    p

---
+++ 13.js
@@ -0,0 +1,10 @@
+let input=document.getElementsByClassName("input");
+let text = document.getElementsByTagName("p");
+let add = document.getElementsByClassName("add");
+let delete = document.getElementsByClassName("delete");
+
+function addTask(){
+    if(input==Text){
+        
+    }
+}
