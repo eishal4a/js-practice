216c225a8226d33e3063c74e742f6467ccbcc89e
Eishal <eishal.cecos@gmail.com>
2025-05-03 20:22:24 +0500

    p
    p

---
+++ 25.js
@@ -0,0 +1,7 @@
+let btn = document.createElement("button");
+btn.innerText="Click me ";
+
+btn.style.color="red";
+btn.style.backgroundColor="black";
+
+document.querySelector("body").prepend("btn");
